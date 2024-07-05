0de9d08ea1635b3488feb9e39303ba04d19a4347
mashfeii_main <evil-corporaty@yandex.ru>
2024-07-05 12:01:10 +0300

    Add responsive styles to the whole page

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@
 */
 
 // Включить/выключить FLS (Full Logging System) (в работе)
-window["FLS"] = true;
+// window["FLS"] = true;
 
 // Подключение основного файла стилей
 // import "../scss/media.scss";
@@ -25,13 +25,13 @@ import * as flsFunctions from "./files/functions.js";
 
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 /* (i) необходимо для корректного отображения webp из css  */
-// flsFunctions.isWebp();
+flsFunctions.isWebp();
 /* Добавление класса touch для HTML если браузер мобильный */
 // flsFunctions.addTouchClass();
 /* Добавление loaded для HTML после полной загрузки страницы */
 // flsFunctions.addLoadedClass();
 /* Модуль для работы с меню (Бургер) */
-// flsFunctions.menuInit();
+flsFunctions.menuInit();
 /* Учет плавающей панели на мобильных устройствах при 100vh */
 // flsFunctions.fullVHfix();
 
